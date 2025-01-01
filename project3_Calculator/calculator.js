5b44519c7d572edfe484c8ee0a70a79c710866a6
anantakrroy <kr.innovate@gmail.com>
2025-01-01 14:11:20 +0530

    refactored "=" logic

--- project3_Calculator/calculator.js
+++ project3_Calculator/calculator.js
@@ -12,73 +12,77 @@ for (let i = 0; i < p.length; i++) {
 function clickEventListener(event) {
   console.log(event.target.innerText);
   const char = event.target.innerText;
-  if(!["⬅", "+", "−", "×", "÷", "=", "C"].includes(char)) {
-    // add number to display and add number to operands array
+  if (!["⬅", "+", "−", "×", "÷", "=", "C"].includes(char)) {
+    // add character input to display string
     display += char;
     input.value = display;
-    // operands.push(Number(display));
   }
+  // CLEAR
   if (char == "C") {
     display = "0";
     input.value = display;
     operands = [];
     operator = "";
   }
+  // BACKSPACE
   if (char == "⬅") {
     // keep deleting the chars from display 1 character at a time with
     // newest char deleted first
     display = display.substring(0, display.length - 1);
     input.value = display;
-    // if the display string becomes empty, make sure the last element
-    // from operands array is removed
+    // if the display string becomes empty, set the display to 0
     if (display == "") {
-      operands = operands.pop();
+      display = "0";
+      input.value = display;
     }
   }
+  // OPERATOR CLICKED
   if (["+", "−", "×", "÷"].includes(char)) {
     operands.push(Number(display));
     display = "";
     operator = char;
     input.value = operator;
   }
+  // EQUAL CLICKED
+  // if "=" is pressed, there are two situations
+  // 1. = is pressed without any OPERATION performed, then return the number input in the display
+  // 2. = is pressed after OPERATION is performed and operator is non empty, then return result of "calculateResult"
   if (char == "=") {
-    operands.push(Number(display));
-    input.value = calculateResult("=");
-    operands = [];
-    operator = "";
-    display = "";
+    if (!operator) {
+      input.value = display;
+    } else {
+      operands.push(Number(display));
+      input.value = calculateResult();
+      // after result is calculated 
+    }
+
   }
 }
 
-function calculateResult(op) {
+function calculateResult() {
   console.log(`Current operands >>> ${operands} and operator >> ${operator}`);
 
-  // if '=' is pressed and only one operand is present , then return the operand
-  if (operands.length == 1) {
-    if (op == "=") {
-      return operands[0];
-    }
-  } else {
-    // SUM
-    if (operator == "+") {
-      // console.log(operands.reduce((a, c) => a + c));
-      operands = [operands.reduce((a, c) => a + c)];
-      return operands[0];
-    }
-    // DIFF
-    if (operator == "−") {
-      operands = [operands.reduce((a, c) => a - c)];
-      return operands[0];
-    }
-    // MULTIPLY
-    if (operator == "×") {
-      operands = [operands.reduce((a, c) => a * c)];
-      return operands[0];
-    }
-    // DIVISION
-    if (operator == "÷") {
-      operands = [operands.reduce((a, c) => a / c)];
-      return operands[0];
-    }
+  // SUM
+  if (operator == "+") {
+    // console.log(operands.reduce((a, c) => a + c));
+    operands = [operands.reduce((a, c) => a + c)];
+    console.log(`Operands after SUM >> ${operands}`);
+
+    return operands[0];
+  }
+  // DIFF
+  if (operator == "−") {
+    operands = [operands.reduce((a, c) => a - c)];
+    return operands[0];
+  }
+  // MULTIPLY
+  if (operator == "×") {
+    operands = [operands.reduce((a, c) => a * c)];
+    return operands[0];
+  }
+  // DIVISION
+  if (operator == "÷") {
+    operands = [operands.reduce((a, c) => a / c)];
+    return operands[0];
   }
 }
