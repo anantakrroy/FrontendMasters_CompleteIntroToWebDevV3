3f5030c4b0f39c341ad7894b01cbe7e4f22599a2
anantakrroy <kr.innovate@gmail.com>
2025-01-11 16:11:57 +0530

    working game app

--- project4_WordMaster/game.js
+++ project4_WordMaster/game.js
@@ -1,9 +1,10 @@
 const rows = document.querySelectorAll(".row");
 const numAttempts = document.querySelector(".puzzle").childElementCount;
-const dayWordUrl = "https://words.dev-apis.com/word-of-the-day";
+const dayWordUrl = "https://words.dev-apis.com/word-of-the-day?puzzle=361";
 let wordOfTheDay = "";
-let numOfChars = {};
 let userInputWord = "";
+let currentAttempt = 0;
+let result = false;
 
 // GET word from API
 async function getWordOfDay() {
@@ -41,26 +42,29 @@ async function validateWord(word) {
     "https://words.dev-apis.com/validate-word",
     options
   );
+
   const response = await request.json();
-
-  // console.log("POST response >>>>> ", response);
   endAnimation();
-  return response;
 
+  return response;
+}
+
+// Add event listener to all rows
+for (let i = 0; i < rows.length; i++) {
+  rows[i].addEventListener("keydown", handleRow);
 }
 
 // Handle attempts
-for (let i = 0; i < rows.length; i++) {
-  // console.log("ROW Number >> ", i + 1);
-  rows[i].addEventListener("keydown", handleRow);
-}
+// while(currentAttempt <= numAttempts) {
+
+// }
 
 async function handleRow(e) {
   // Stop bubbling event to adjacent input and prevent default browser behavior for input field focus
   e.stopPropagation();
   e.preventDefault();
 
-  // console.log(e.target);
+  console.log(e.currentTarget);
   console.log(`Pressed >> ${e.key} with code >> ${e.key.charCodeAt(0)}`);
 
   // Construct array of children of event.target
@@ -73,7 +77,6 @@ async function handleRow(e) {
   userInputWord = children.reduce((a, c) => a + c.value, "");
 
   if (checkAlphabet(e.key) && e.key.length == 1) {
-    // console.log(`Focused on child : ${children.indexOf(e.target) + 1}`);
     handleCharKey(e, nthChild);
   } else if (e.key == "Backspace") {
     handleBackspaceKey(e, nthChild, children);
@@ -119,18 +122,22 @@ async function handleRow(e) {
   async function handleEnterKey(event, nthChild, children) {
     // Remove the css class to ensure red outline is shown again
     children.map((el) => el.removeAttribute("class", "redanimate"));
+
     // send POST request to API and check if its a valid word
     // if it is valid , compare with word of the day
     // else show red borders around all the input borders
-    // console.log(children.reduce((a,c) => a + c.value, ''))
     // finally shift focus to 2nd attempt i.e 2nd row
-    // const userInputWord = children.reduce((a, c) => a + c.value, "");
+    
     const isValidWord = await validateWord(userInputWord);
     console.log("Response from server >>> ", isValidWord);
+    
     if (!isValidWord.validWord) {
       invalidWord(children);
     } else {
+      children.forEach((el) => el.setAttribute("disabled", "disabled"));
       validWord(isValidWord.word, children);
+      currentAttempt++;
+      handleNextAttempt(currentAttempt);
     }
   }
 }
@@ -140,8 +147,7 @@ function invalidWord(children) {
 }
 
 function validWord(word, children) {
-  // let numOfCharsUserWord = letterOccurence(word);
-  // numOfChars = letterOccurence(wordOfTheDay);
+  // Keep track of chars already seen
   let seen = [];
 
   for (let i = 0; i < word.length; i++) {
@@ -149,81 +155,62 @@ function validWord(word, children) {
     const regex = new RegExp(currentChar, "gi");
     seen.push(currentChar);
 
-    const currentCharIdx = findIndices(currentChar, wordOfTheDay);
-    const currentCharIdxUser = findIndices(currentChar, userInputWord);
-
-    console.log(
-      `Character ${word[i]} present in word of the day at ${currentCharIdx}`
-    );
-
-    console.log(
-      `Character ${word[i]} present in user word at ${currentCharIdxUser}`
-    );
-
     // Char does not exist in word
 
     if (!wordOfTheDay.includes(currentChar)) {
       children[i].setAttribute("class", "gray");
-    } else {
-      // Char exists
-      // Char appears only once in the word of day
+    }
+
+    // User guessed entire word correctly
+    else if(word === wordOfTheDay) {
+      children.forEach(el => el.setAttribute("class", "green"));
+      result = true;
+      showGreetingMessage(result);
+    }
+
+    // Char exists
+    else {
+      // Case 1 --> current char in guessed word is less or equal to that in actual word
       if (
-        seen.filter((el) => el == currentChar).length ==
+        seen.filter((el) => el == currentChar).length <=
         wordOfTheDay.match(regex).length
       ) {
-        if (word.indexOf(currentChar) == wordOfTheDay.indexOf(currentChar)) {
+        if (
+          word.slice(i).indexOf(currentChar) ===
+          wordOfTheDay.slice(i).indexOf(currentChar)
+        ) {
           children[i].setAttribute("class", "green");
         } else {
           children[i].setAttribute("class", "yellow");
         }
       }
-      if (
-        seen.filter((el) => el == currentChar).length >
-        wordOfTheDay.match(regex).length
-      ) {
+      // Case 2 -> current char in guessed word exceeds that in actual word, so we need to ignore them
+      else {
         children[i].setAttribute("class", "gray");
       }
     }
   }
 }
 
-// Compare char positions between user word and word of the day
-function comparePosOfChar(arrA, arrB) {
-  let numIterations = arrA.length;
-  if (arrA.length === arrB.length) {
-    for (let el of arrA) {
-      return arrB.indexOf(el) === arrA.indexOf(el);
-    }
-  }
-  if (arrA.length < arrB.length) {
-    let rejectedIdx = arrB.slice(arrA.length);
+function handleNextAttempt(num) {
+  if (num < numAttempts) {
+    rows[currentAttempt].children[0].focus();
+  } else {
+    showGreetingMessage(result);
   }
 }
 
-// find all indices of char in word of the day
-function findIndices(char, word) {
-  let idxArr = [];
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === char) {
-      idxArr.push(i);
-    }
+function showGreetingMessage(result) {
+  const element = document.querySelector(".greeting");
+  if (!result) {
+    element.classList.add("red");
+    element.innerText = `You lose ! Correct word : ${wordOfTheDay}`;
+  } else {
+    document.querySelectorAll('input').forEach(el => el.setAttribute('disabled','disabled'));
+    element.classList.add("green");
+    element.innerText = `You guessed it right ! Correct word : ${wordOfTheDay}`;
   }
-  return idxArr;
-}
-
-// Count the occurence of letters
-// console.log("occ. obj >>> ", numOfChars);
-
-function letterOccurence(word) {
-  let occurences = {};
-  for (let char of word) {
-    if (occurences[char]) {
-      occurences[char]++;
-    } else {
-      occurences[char] = 1;
-    }
-  }
-  return occurences;
+  element.classList.remove("hidden");
 }
 
 function checkAlphabet(char) {
